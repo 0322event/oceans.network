2a57aeb1369d59377a1f82a8b54d6799b6f98bf9
0322event <60565708+0322event@users.noreply.github.com>
2020-03-04 22:58:00 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function( $ ) {
+
+  // Smooth scroll for the menu and links with .scrollto classes
+  $('.smoothscroll').on('click', function(e) {
+    e.preventDefault();
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - 48
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $(".navbar-collapse a").on('click', function() {
+    $(".navbar-collapse.collapse").removeClass('in');
+  });
+	
+			$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 400){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
+	
+});
+
